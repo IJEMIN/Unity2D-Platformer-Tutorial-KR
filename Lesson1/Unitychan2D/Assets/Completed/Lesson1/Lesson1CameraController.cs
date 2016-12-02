b03d760d2eebd965bb3449d7abd1da791d2d037c
I_Jemin <i_jemin@hotmail.com>
2016-12-03 02:05:42 +0900

    Add Lesson 1\nMoving Character and Cam

---
+++ Lesson1/Unitychan2D/Assets/Completed/Lesson1/Lesson1CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Lesson1CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    private Camera m_camera;
+
+    void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        var right = m_camera.ViewportToWorldPoint(Vector2.right);
+        var center = m_camera.ViewportToWorldPoint(Vector2.one * 0.5f);
+
+        if (center.x < target.position.x)
+        {
+            var pos = m_camera.transform.position;
+
+            if (Math.Abs(pos.x - target.position.x) > 0f)
+            {
+                m_camera.transform.position = new Vector3(target.position.x, pos.y, pos.z);
+            }
+        }
+    }
+}
---
+++ Lesson1/Unitychan2D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    private Camera m_camera;
+
+    void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+    }
+
+    void LateUpdate()
+    {
+        var right = m_camera.ViewportToWorldPoint(Vector2.right);
+        var center = m_camera.ViewportToWorldPoint(Vector2.one * 0.5f);
+
+
+    }
+}
